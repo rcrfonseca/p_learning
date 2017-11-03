acf9385dc1ddad0016574a6115854cd465ceb4e5
Raphael Fonseca <kerberuz@gmail.com>
2017-11-03 18:41:21 -0200

    removing-folder-useless-tutorial

---
+++ SNA/tweepy-geo-test-place.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+import tweepy
+
+auth = tweepy.OAuthHandler("Ur5J5gHT6uYg7zms7GaLUNYWf","SXSyveGpYiTzjifwC84IZ7IJBP7Lx138nODPhGW7CkykjIKTyu")
+auth.set_access_token("2471507719-kvni00S7JySj8Iq3PUD80d01jrh8HpSodLnoABo","EjbRRnAf9E8CgedjX9qBZ2XZABnRGI8vHeHft50CU6f8S")
+
+api = tweepy.API(auth)
+places = api.geo_search(query="BR", granularity="city")
+place_id = places[0].id
+
+tweets = api.search(q="place:%s" % place_id)
+for tweet in tweets:
+    print tweet.text + " | " + tweet.place.name + " | " + tweet.place.id if tweet.place else "Undefined place"
