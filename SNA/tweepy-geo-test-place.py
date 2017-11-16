2d3f4b4db50282fd951964aa3763d7cbfc8358b0
Raphael Fonseca <kerberuz@gmail.com>
2017-11-16 13:17:35 -0200

    add-youtube-script

--- SNA/tweepy-geo-test-place.py
+++ SNA/tweepy-geo-test-place.py
@@ -6,7 +6,7 @@ auth = tweepy.OAuthHandler("Ur5J5gHT6uYg7zms7GaLUNYWf","SXSyveGpYiTzjifwC84IZ7IJ
 auth.set_access_token("2471507719-kvni00S7JySj8Iq3PUD80d01jrh8HpSodLnoABo","EjbRRnAf9E8CgedjX9qBZ2XZABnRGI8vHeHft50CU6f8S")
 
 api = tweepy.API(auth)
-places = api.geo_search(query="BR", granularity="city")
+places = api.geo_search(query="BR", granularity="country")
 place_id = places[0].id
 
 tweets = api.search(q="place:%s" % place_id)
